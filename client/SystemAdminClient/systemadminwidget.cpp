a300631f5bbe37ab8ec05f6709bcf2a671cf6287
hahaleyile <1025284546@qq.com>
2023-06-16 17:06:34 +0800

    Finish system admin client

--- client/SystemAdminClient/systemadminwidget.cpp
+++ client/SystemAdminClient/systemadminwidget.cpp
@@ -1,23 +1,24 @@
 #include <QJsonParseError>
+#include <QJsonObject>
+#include <qdatetime.h>
 #include <qdebug.h>
+#include <qjsonarray.h>
+#include <qjsonvalue.h>
 #include <qmap.h>
+#include <qtimer.h>
+#include <qurl.h>
 #include <qvariant.h>
-#include <stdio.h>
 #include "systemadminwidget.h"
 #include "ui_systemadminwidget.h"
 
 SystemAdminWidget::SystemAdminWidget(QWidget *parent)
 	:
 	QWidget(parent),
-	ui(new Ui::SystemAdminWidget)
+	ui(new Ui::SystemAdminWidget),
+	init_endpoints{"health", "metrics/jvm.memory.max", "metrics/jvm.memory.used", "metrics/system.cpu.count", "env"},
+	endpoints{"health", "metrics/jvm.memory.used"}
 {
 	ui->setupUi(this);
-	system_audit();
-}
-
-void SystemAdminWidget::system_audit()
-{
-
 	if (nullptr == manager) {
 		manager = new QNetworkAccessManager(this);
 		connect(manager, SIGNAL(finished(QNetworkReply * )),
@@ -27,12 +28,23 @@ void SystemAdminWidget::system_audit()
 		connect(manager, SIGNAL(authenticationRequired(QNetworkReply * , QAuthenticator * )),
 				this, SLOT(slot_provideAuthenication(QNetworkReply * , QAuthenticator * )));
 	}
+
+	system_audit();
+	init = false;
+
+	QTimer *timer = new QTimer(this);
+	connect(timer, &QTimer::timeout, this, &SystemAdminWidget::system_audit);
+	timer->start(3000);
+}
+
+void SystemAdminWidget::system_audit()
+{
 	QNetworkRequest request;
 	QSslConfiguration config;
+	QString url = "https://81.69.243.226:6500/monitor/";
 	config.setPeerVerifyMode(QSslSocket::VerifyNone);
 	config.setProtocol(QSsl::TlsV1_2);
 	request.setSslConfiguration(config);
-	request.setUrl(QUrl("https://81.69.243.226:6500/monitor/health"));
 	QString input = username + ":" + password;
 	char *inputbyte = (input.toUtf8().data());
 	long inputlen;
@@ -51,11 +63,19 @@ void SystemAdminWidget::system_audit()
 	base64enc(output, inputbyte, inputlen);
 	QString authorizationstr = QString("Basic ") + QString(output);
 	request.setRawHeader("Authorization", authorizationstr.toUtf8());
-	post_reply = manager->get(request);
+	if (init)
+		for (auto endpoint: init_endpoints) {
+			request.setUrl(QUrl(url + endpoint));
+			post_reply = manager->get(request);
+		}
+	else
+		for (auto endpoint: endpoints) {
+			request.setUrl(QUrl(url + endpoint));
+			post_reply = manager->get(request);
+		}
 
 	connect(post_reply, SIGNAL(error(QNetworkReply::NetworkError)),
 			this, SLOT(slot_NetWorkError(QNetworkReply::NetworkError)));
-
 }
 
 void SystemAdminWidget::slot_replyFinished(QNetworkReply *reply)
@@ -104,8 +124,77 @@ void SystemAdminWidget::slot_replyFinished(QNetworkReply *reply)
 						->setText("磁盘已使用空间：" + dataConvert(total.toLongLong() - free.toLongLong()));
 					ui->listWidget->item(2)->setText("磁盘未使用空间：" + dataConvert(free.toLongLong()));
 					double percent = 1 - ((double)free.toLongLong()) / total.toLongLong();
-					qDebug() << Q_FUNC_INFO << "percent is " << percent;
 					ui->progressBar->setValue(percent * 100);
+				}
+			}
+		}
+
+			// JVM 内存和 CPU 个数
+		else if (reply->url().toString().contains("metrics")) {
+			QByteArray resultjsonbytearray;
+			QJsonParseError parseresult;
+			resultjsonbytearray.append(ret_data);
+			QJsonDocument parse_doucment = QJsonDocument::fromJson(resultjsonbytearray, &parseresult);
+			if (parseresult.error == QJsonParseError::NoError) {
+				if (parse_doucment.isObject()) {
+					QVariant mem;
+					QJsonValue v = parse_doucment.object().value("measurements");
+					mem = v.toArray()[0].toObject().value("value");
+
+					if (reply->url().toString().contains("jvm.memory.max")) {
+						maxmem = mem;
+						ui->listWidget_2->item(0)->setText("JVM最大内存：" + dataConvert(mem.toLongLong()));
+					}
+
+					else if (reply->url().toString().contains("jvm.memory.used")) {
+						ui->listWidget_2->item(1)->setText("JVM已使用内存大小: " + dataConvert(mem.toLongLong()));
+						if (!maxmem.isNull()) {
+							ui->progressBar_2->setValue(100 * (mem.toDouble() / maxmem.toDouble()));
+							ui->listWidget_2->item(2)
+								->setText("JVM可用内存大小：" + dataConvert(maxmem.toLongLong() - mem.toLongLong()));
+						}
+					}
+
+					else if (reply->url().toString().contains("system.cpu.count")) {
+						ui->listWidget_3->item(0)->setText("系统CPU个数：" + QString::number(mem.toDouble()));
+					}
+				}
+			}
+		}
+
+			// 系统信息
+		else if (reply->url().toString().contains("env")) {
+			QByteArray resultjsonbytearray;
+			QJsonParseError parseresult;
+			resultjsonbytearray.append(ret_data);
+			QJsonDocument parse_doucment = QJsonDocument::fromJson(resultjsonbytearray, &parseresult);
+			if (parseresult.error == QJsonParseError::NoError) {
+				if (parse_doucment.isObject()) {
+					QVariant mem;
+					QJsonValue v = parse_doucment.object().value("propertySources");
+					for (const auto obj: v.toArray()) {
+						if (obj.toObject().value("name") == "server.ports") {
+							auto port =
+								obj.toObject().value("properties")
+									.toObject().value("local.server.port")
+									.toObject().value("value");
+							ui->listWidget_3->item(3)->setText("端口：" + QString::number(port.toDouble()));
+						}
+
+						else if (obj.toObject().value("name") == "systemProperties") {
+							auto val =
+								obj.toObject().value("properties")
+									.toObject().value("java.runtime.version")
+									.toObject().value("value");
+							ui->listWidget_3->item(1)->setText("JAVA版本：" + val.toString());
+
+							val =
+								obj.toObject().value("properties")
+									.toObject().value("PID")
+									.toObject().value("value");
+							ui->listWidget_3->item(4)->setText("进程号：" + val.toString());
+						}
+					}
 				}
 			}
 		}
@@ -157,9 +246,9 @@ QString SystemAdminWidget::dataConvert(long long int data)
 	double result = data;
 	QString units[] = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
 	int cnt = 0;
-	while (result > 100) {
-		result /= 1000;
+	while (result > 128) {
+		result /= 1024;
 		cnt++;
 	}
-	return QString::number(result, 'g', 2) + units[cnt];
+	return QString::number(result, 'g', 3) + units[cnt];
 }
